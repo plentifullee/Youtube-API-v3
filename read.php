c34a4c79190703efab99a830e3b933975d3db9d5
plentifullee <plentifullee@gmail.com>
2014-06-11 14:44:27 +0800

    11_06_2014
    Examples using tokens

--- read.php
+++ read.php
@@ -1,4 +1,7 @@
 <?php
+//YouTube API v3.0
+//This example will retrieve a set of results in JSON format from a youtube playlist
+//For the in depth tutorial, go to http://plenty.codes
 
 $playlist 	= "replace_me"; //replace with a youtube playlist ID
 $api_key 	= "replace_me"; //your public access API key from google's developer console
@@ -15,20 +18,16 @@ for($i = 0; $i < $maxResults; $i++) {
 	$video_thumbnail_default 	= $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'default'}->{'url'};
 	$video_thumbnail_medium 	= $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'medium'}->{'url'};
 	$video_thumbnail_high 		= $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'high'}->{'url'};
-	$video_thumbnail_standard 	= $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'standard'}->{'url'};
-	$video_thumbnail_maxres 	= $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'maxres'}->{'url'};
 
 	//output the results
 	echo "<b>Title: </b>".$video_title."<br>";
 	echo "<b>Description: </b>".$video_description."<br>";
 	echo "<b>Thumbnail sizes: </b><a href='".$video_thumbnail_default."' target='_blank'>Default</a>, ";
 	echo "<a href='".$video_thumbnail_medium."' target='_blank'>Medium</a>, ";
-	echo "<a href='".$video_thumbnail_high."' target='_blank'>High</a>, ";
-	echo "<a href='".$video_thumbnail_standard."' target='_blank'>Standard</a>, ";
-	echo "<a href='".$video_thumbnail_maxres."' target='_blank'>Hi-Res</a><br><br>";
+	echo "<a href='".$video_thumbnail_high."' target='_blank'>High</a><br><br>";
 }
 
 //output the path used
-echo "<br>This is the feed URL: <br><a href='".$path."' target='_blank'>".$path."</a>";
+echo "<b>Feed URL:</b> <a href='".$path."' target='_blank'>".$path."</a>";
 
 ?>
