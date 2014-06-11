c6e1c4f10d8cb660513977c045654dee0ff98220
plentifullee <plentifullee@gmail.com>
2014-06-11 22:16:36 +0800

    11_06_2014
    Optimized token.php

--- read.php
+++ read.php
@@ -1,10 +1,10 @@
 <?php
 //YouTube API v3.0
-//This example will retrieve a set of results in JSON format from a youtube playlist
+//Examples of retrieving and processing a JSON format youtube feed including thumbnails
 //For the in depth tutorial, go to http://plenty.codes
 
-$playlist 	= "replace_me"; //replace with a youtube playlist ID
-$api_key 	= "replace_me"; //your public access API key from google's developer console
+$playlist = "replace_me"; //replace with a youtube playlist ID
+$api_key = "replace_me"; //your public access API key from google's developer console
 $maxResults = "5";
 
 $path = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=$maxResults&playlistId=$playlist&key=$api_key";
@@ -12,12 +12,12 @@ $feed = json_decode(file_get_contents($path));
 
 for($i = 0; $i < $maxResults; $i++) {
 	//retrieve video information
-	$video_title 				= $feed->{'items'}[$i]->{'snippet'}->{'title'};
-	$video_description 			= $feed->{'items'}[$i]->{'snippet'}->{'description'};
+	$video_title = $feed->{'items'}[$i]->{'snippet'}->{'title'};
+	$video_description = $feed->{'items'}[$i]->{'snippet'}->{'description'};
 	//retrieve thumbnail sizes
-	$video_thumbnail_default 	= $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'default'}->{'url'};
-	$video_thumbnail_medium 	= $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'medium'}->{'url'};
-	$video_thumbnail_high 		= $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'high'}->{'url'};
+	$video_thumbnail_default = $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'default'}->{'url'};
+	$video_thumbnail_medium = $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'medium'}->{'url'};
+	$video_thumbnail_high = $feed->{'items'}[$i]->{'snippet'}->{'thumbnails'}->{'high'}->{'url'};
 
 	//output the results
 	echo "<b>Title: </b>".$video_title."<br>";
@@ -29,5 +29,3 @@ for($i = 0; $i < $maxResults; $i++) {
 
 //output the path used
 echo "<b>Feed URL:</b> <a href='".$path."' target='_blank'>".$path."</a>";
-
-?>
---
+++ token.php
@@ -0,0 +1,52 @@
+<?php
+//YouTube API v3.0
+//Examples of using tokens to retrieve the next / previous set of feeds
+//For the in depth tutorial, go to http://plenty.codes
+
+$playlist = "replace_me"; //replace with a youtube playlist ID
+$api_key = "replace_me"; //your public access API key from google's developer console
+$maxResults = "5"; 
+
+$path = getPath();
+$feed = json_decode(file_get_contents($path));
+echoResults($feed);
+$nextToken = $feed->{'nextPageToken'}; //get the next token
+echo "Next page token: <b>".$nextToken."</b><br><br>";
+echo "<b>Feed URL:</b> <a href='".$path."' target='_blank'>".$path."</a><br><br>";
+
+echo "<hr>";
+
+echo "<i>Using the next page token, we can retrieve the next set of results.</i><br><br>";
+$path = getPath($nextToken); //use the next token on the path
+$feed = json_decode(file_get_contents($path));
+echoResults($feed);
+$nextToken = $feed->{'nextPageToken'};
+$prevToken = $feed->{'prevPageToken'}; //we can retrieve a previous token
+echo "Next page token: <b>".$nextToken."</b><br>";
+echo "Previous page token: <b>".$prevToken."</b><br><br>";
+echo "<b>Feed URL:</b> <a href='".$path."' target='_blank'>".$path."</a><br><br>";
+
+//sets the feed path, if next token exist, append it to the path
+function getPath($nextToken = FALSE) {
+	if($nextToken === FALSE)
+		$nextPageToken = "";
+	else
+		$nextPageToken = "&pageToken=$nextToken";
+	
+	global $playlist, $api_key, $maxResults;
+	$path = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=$maxResults&playlistId=$playlist".$nextPageToken."&key=$api_key";
+	return $path;
+}
+
+//gets specific results from the json feed and echo
+function echoResults($feed) {
+	global $maxResults;
+	for($i = 0; $i < $maxResults; $i++) {
+		//retrieve video information
+		$video_title = $feed->{'items'}[$i]->{'snippet'}->{'title'};
+		$video_description = $feed->{'items'}[$i]->{'snippet'}->{'description'};
+		//output the results
+		echo "<b>Title: </b>".$video_title."<br>";
+		echo "<b>Description: </b>".$video_description."<br><br>";
+	}	
+}
